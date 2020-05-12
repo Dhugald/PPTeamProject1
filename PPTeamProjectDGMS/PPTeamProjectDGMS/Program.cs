d40ad8aab12f99422f73191280f6647508df3bab
Dhugald Spry <sprydl1@student.op.ac.nz>
2020-05-13 10:10:52 +1200

    Added the start for the therapist, basic conversation with key words

--- PPTeamProjectDGMS/PPTeamProjectDGMS/Program.cs
+++ PPTeamProjectDGMS/PPTeamProjectDGMS/Program.cs
@@ -10,7 +10,31 @@ namespace PPTeamProjectDGMS
     {
         static void Main(string[] args)
         {
-            Dhugald();
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine($"So {name}, What brings you in today?");
+            string answer1 = Console.ReadLine().ToLower();
+            if (answer1.Contains("family") || answer1.Contains("parent") || answer1.Contains("sibling"))
+            {
+
+            }
+            else if(answer1.Contains("friends"))
+            {
+
+            }
+            else if (answer1.Contains("relationships"))
+            {
+
+            }
+            else
+            {
+                Dhugald();
+            }
+
+
+            
         }
         static void Glen()
         {
