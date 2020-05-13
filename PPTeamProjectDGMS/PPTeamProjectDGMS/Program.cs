779d70e97d82294f1e8641b6fcc0142ca55996d1
GlenWOP <61264976+GlenWOP@users.noreply.github.com>
2020-05-13 14:44:46 +1200

    Added code to Glen's Section
    Use this code for you're own sections.
    Line 108 - Need to find a way to take the user back to the opening question after saying else.

--- PPTeamProjectDGMS/PPTeamProjectDGMS/Program.cs
+++ PPTeamProjectDGMS/PPTeamProjectDGMS/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +14,7 @@ namespace PPTeamProjectDGMS
             string name = Console.ReadLine();
             Console.Clear();
 
-            Console.WriteLine($"So {name}, What brings you in today?");
+            Console.WriteLine($"Therapist: Hello {name}, my name is Judy and I'm a therapist specializing in relatioships, What brings you in today?");
             string answer1 = Console.ReadLine().ToLower();
             if (answer1.Contains("family") || answer1.Contains("parent") || answer1.Contains("sibling"))
             {
@@ -39,10 +39,83 @@ namespace PPTeamProjectDGMS
         static void Glen()
         {
 
-            Console.WriteLine("Test family");
+            Console.WriteLine("Therapist: I see, Is there a particular member of you're family that is on you're mind today?");
+            string family1 = Console.ReadLine().ToLower();
+            if (family1.Contains("mother") || family1.Contains("mom") || family1.Contains("mum"))
+            {
+                Mother();
+            }
+            else if (family1.Contains("father") || family1.Contains("dad"))
+            {
+                Father();
+            }
+            else if (family1.Contains("sister"))
+            {
+                Sister();
+            }
+            else if (family1.Contains("brother"))
+            {
+                Brother();
+            }
+            else
+            {
+                FamilyOther();
+            }
+
+           
+         
+        }
+
+        static void Mother()
+        {
+            Console.WriteLine("Therapist: Do you have a good relationship with you're Mother?");
             Console.ReadLine();
 
+
         }
+        
+        static void Father()
+        {
+            Console.WriteLine("Therapist: Do you have a good relationship with you're Mother?");
+            string father1 = Console.ReadLine().ToLower();
+
+        }
+
+        static void Brother()
+        {
+            Console.WriteLine("Therapist: Do you have a good relationship with you're Mother?");
+            string brother1 = Console.ReadLine().ToLower();
+
+        }
+
+        static void Sister()
+        {
+            Console.WriteLine("Therapist: Do you have a good relationship with you're Sister?");
+            string sister1 = Console.ReadLine().ToLower();
+
+        }
+
+        static void FamilyOther()
+        {
+            Console.WriteLine("Therapist: Do you consider this person a family member?");
+            string familyOther1 = Console.ReadLine().ToLower();
+            if (familyOther1.Contains("yes") || familyOther1.Contains("yeah"))
+                {
+                FamilyOtherYes();
+            }
+            else
+            {
+                // Take them back to the first Question that asks them what they are here for
+            }
+
+        }
+
+        
+        static void FamilyOtherYes()
+        {
+            Console.WriteLine("Therapist: TO BE ADDED TOO");
+        }
+
 
         static void Saniya()
         {
